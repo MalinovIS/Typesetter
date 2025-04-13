82e8c97c94e730e6f502e4a90f87f7ee46ae1996
MalinovIS <49347069+MalinovIS@users.noreply.github.com>
2025-04-13 10:57:40 +0300

    Initial Commit

---
+++ Typesetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Typesetter
+//
+//  Created by Илья Малинов on 13.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
